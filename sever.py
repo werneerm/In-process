565234e7742c2235d95033a1dfb957f5f11a1ea3
HaveaKillerDay <petho.adam911@gmail.com>
2020-01-06 13:38:40 +0100

    alapok

--- sever.py
+++ sever.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    question = data_handler.get_all_question()
-
+    question = data_handler.get_all_questions()
     return render_template('list.html', question=question)
 
 
