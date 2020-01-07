ec854c8bbd42433a1465c98c52a0e907902027b7
werneerm <wmarcos25@gmail.com>
2020-01-07 17:44:54 +0100

    redirect to the specified question

--- sever.py
+++ sever.py
@@ -29,10 +29,26 @@ def questions_site(id = None):
         answer = data_handler.index_finder(id)
         return render_template('/questions.html', answer=answer,id=id)
 
-@app.route('/questions/<int:id>/answer-question', methods=['GET','POST'])
+
+@app.route('/questions/<int:id>', methods=['GET', 'POST'])
+@app.route('/questions/<int:id>/answer-question', methods=['GET', 'POST'])
 def add_answer(id=None):
-    if id is not None:
-        return render_template('/new_answer.html')
+    if request.method == 'GET':
+        return render_template('/new_answer.html',id=id)
+    if request.method == 'POST':
+        message = request.form['message']
+        table = data_handler.get_all_answer()
+        new_answer_list = []
+        new_answer_list.append('3')
+        new_answer_list.append('submission time')
+        new_answer_list.append('vote number')
+        new_answer_list.append(str(id))
+        new_answer_list.append(message)
+        new_answer_list.append('image')
+        table.append(new_answer_list)
+        data_handler.write_user_story(DATA_FILE_PATH_ANSWER, table)
+        answer = data_handler.index_finder(id)
+        return render_template('/questions.html',answer=answer,id=id)
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def add_question():
