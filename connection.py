77f61e8e2dc15d09aae3f999be8f102885875765
HaveaKillerDay <petho.adam911@gmail.com>
2020-01-20 14:04:39 +0100

    environment variable

--- connection.py
+++ connection.py
@@ -5,10 +5,10 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = 'adam'
-    password = 'madafaka911'
-    host = 'localhost'
-    database_name = 'ask-mate'
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
 
     env_variables_defined = user_name and password and host and database_name
 
