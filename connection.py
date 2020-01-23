e863344d66479e3870e8c2f61271fe5576d4efc1
andras.holczbauer <holcz08@gmail.com>
2020-01-23 10:02:44 +0100

    let me pull

--- connection.py
+++ connection.py
@@ -5,10 +5,10 @@ import psycopg2.extras
 def get_connection_string():
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = ('mark')
-    password = ('filekezgya25')
+    user_name = ('Kisholcz')
+    password = ('AcerSwift3')
     host = ('localhost')
-    database_name =('mark')
+    database_name =('Whatever')
 
     env_variables_defined = user_name and password and host and database_name
 
